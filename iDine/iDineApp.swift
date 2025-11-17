04fc7c51a30dedbaf1e17e256dc0e4e86e4634de
hughmcmillanv <scotmcmillan@mac.com>
2025-11-17 11:25:49 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Hugh McMillan V on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
